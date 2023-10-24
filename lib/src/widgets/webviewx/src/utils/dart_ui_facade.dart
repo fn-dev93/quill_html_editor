a579a365ed65b6e6370384d121f8735864594295
fn-dev93 <82242343+fn-dev93@users.noreply.github.com>
2023-10-24 11:40:34 -0300

    Update dart_ui_facade.dart

--- lib/src/widgets/webviewx/src/utils/dart_ui_facade.dart
+++ lib/src/widgets/webviewx/src/utils/dart_ui_facade.dart
@@ -1,10 +1,13 @@
+import 'dart:ui_web' as ui;
+
 /// This is here just to suppress the missing warning from "web.dart".
 // ignore: camel_case_types
 class platformViewRegistry {
   /// See https://github.com/flutter/flutter/issues/41563 for more info
   static void registerViewFactory(
     String viewId,
-    dynamic Function(int viewId) callback,
-  ) =>
-      throw UnimplementedError();
+    dynamic Function(int viewId) cb,
+  ) {
+    ui.platformViewRegistry.registerViewFactory(viewId, cb);
+  }
 }
